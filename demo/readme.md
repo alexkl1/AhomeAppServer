23ae634c953148d4d2ab8123ecffdf180dc49723
alexander <alexkl1@ya.ru>
2023-02-20 16:16:52 +0300

    demo screenshot

--- demo/readme.md
+++ demo/readme.md
@@ -1,1 +1,16 @@
 This is fixed snapshots for demo account
+snapshot file names should match following format
+
+
+<camid>_<sampleid>.jpeg
+
+camid: string camera identifier (configured in database for demo account)
+sampleid: screenshot sample starting from 1 (1-99)
+
+example screenshots
+
+cam1_1.jpeg
+cam2_1.jpeg
+cam2_2.jpeg
+
+server autoiterates folders and refresh file structure 
--- routes/snapshot.js
+++ routes/snapshot.js
@@ -3,12 +3,73 @@
  * in this implementation returns jpeg snapshot from camera on request
  */
 const express = require('express');
+const fs = require("fs");
 const router = express.Router();
 const getAuth = require("./auth/auth").getAuth;
 const axios = require("axios").default;
+
+const getDemoFile = (cameraId, fileIndex) => {
+    const demoFolder  = 'demo/';
+    return `${demoFolder}${cameraId}_${fileIndex}.jpeg`;
+}
+
+/**
+ * TODO: refactor to better storage ?
+ * @type {Map<any, any>}
+ */
+let demoFileMap = new Map(); // map holds last queried file index of screenshot for given camera. reset to 1 if no more sample screenshots
+/**
+ * Iteractively return screenshots from demo cameras
+ * @param camerdId - cameraid (example: cam1)
+ * @returns {Generator<*, Promise<unknown>, *>}
+ */
+function demoScreenShot(cameraId) {
+    const fs = require ('fs');
+    while (true) {
+        //const cameraId  = yield;
+        console.log("demoscreenshot: ", cameraId);
+        console.log("state: ", demoFileMap);
+        let fcontent = null; // buffer to read
+        let fileIdx = 1; // default file index
+        let fileExists = false;
+
+        if (demoFileMap.has(cameraId)) {
+            // got last cached screenshot
+            fileIdx = Number(demoFileMap.get(cameraId))+1;
+            fileExists = fs.existsSync(getDemoFile(cameraId, fileIdx));
+            if (!fileExists)
+            {
+                // reset to first file
+                fileIdx = 1;
+                fileExists = fs.existsSync(getDemoFile(cameraId, fileIdx));
+            }
+        }
+        else
+        {
+            // check first index file
+            fileExists = fs.existsSync(getDemoFile(cameraId, fileIdx));
+        }
+
+        if (fileExists) {
+            fcontent = fs.readFileSync(getDemoFile(cameraId, fileIdx), {encoding: 'binary'});
+            console.log("buffer: ", fcontent?.length);
+            demoFileMap.set(cameraId, fileIdx);
+            console.log("newstate: ", demoFileMap);
+            return fcontent;
+        }
+        else return null;
+    }
+};
+
 /**
  * @TODO use authentication middleware
  */
+
+/**
+ * Get snapshot from camera (according to config in configuration database)
+ * if snapshoturl is 'demo' then snapshots are taken from local folder
+ */
+const get = getDemoFile()
 router.get('/', async function (req, res, next) {
     //console.log("Snapshot ", req?.headers);
     if (req?.query?.id) {
@@ -19,6 +80,23 @@ router.get('/', async function (req, res, next) {
                 try {
                     const camconfig = JSON.parse(token?.cameraconfig).find(i => i?.id === req?.query?.id);
                     //console.log("Cam config = ", camconfig);
+
+                    // check if demo account
+                    if (camconfig?.snapshotUrl == 'demo')
+                    {
+                        const fcontent = demoScreenShot(camconfig?.id);
+                        //console.log("out fcontent = ",fcontent);
+                        if (fcontent !== null)
+                        {
+                            res.status(200).contentType("image/jpeg").send(fcontent);
+                        }
+                        else
+                        {
+                            res.status(404).send("Screenshot not found");
+                        }
+                        return;
+                    }
+
                     const axres = await axios.get(camconfig?.snapshotUrl,{
                         responseType: 'arraybuffer',
                         timeout: 60000,
@@ -36,7 +114,6 @@ router.get('/', async function (req, res, next) {
         } else res.status(401).send("Not authorized");
     }
     else res.status(400).send("No params");
-
 });
 
 module.exports = router;
